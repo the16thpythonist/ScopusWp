ef6b50016cf9439d29fb964ce90950e95e9daa50
the16thpythonist <jonseb1998@gmail.com>
2017-11-18 13:45:38 +0100

    Changed the internal code structure of the get string method from a lines list approach to a string template format approach AND Adjusted the internal code of the format citations method to use the generalized bullet point function, instead of implementing from scratch

--- scopus_views.py
+++ scopus_views.py
@@ -35,18 +35,20 @@ class PublicationBriefView:
 
     def get_string(self):
 
-        string_lines_list = []
+        title_string = self._format_title_string()
+        published_string = self._format_published_string()
+        description_string = self._format_published_string()
 
-        string_lines_list.append(self._format_title_string())
-        string_lines_list.append(self._format_published_string())
+        string = """
+        {title}
+        {published}
 
-        string_lines_list.append('')
+        {description}
+        """.format(title=title_string,
+                   published=published_string,
+                   description=description_string)
 
-        string_lines_list.append(self._format_description_string())
-
-        string_lines_list.append('')
-
-        return '\n'.join(string_lines_list)
+        return string
 
     def _format_title_string(self):
         title_string = self.publication.title
@@ -76,26 +78,28 @@ class PublicationPrettyView(PublicationBriefView):
 
     def get_string(self):
 
-        string_lines_list = []
+        title_string = self._format_title_string()
+        published_string = self._format_published_string()
+        description_string = self._format_published_string()
+        authors_string = self._format_authors_string()
+        citations_string = self._format_citations_string()
 
-        string_lines_list.append(self._format_title_string())
+        string = """
+                {title}
 
-        string_lines_list.append('')
+                {published}
+                {authors}
 
-        string_lines_list.append(self._format_published_string())
-        string_lines_list.append(self._format_authors_string())
+                {description}
 
-        string_lines_list.append('')
+                {citations}
+                """.format(title=title_string,
+                           published=published_string,
+                           authors=authors_string,
+                           description=description_string,
+                           citations=citations_string)
 
-        string_lines_list.append(self._format_description_string())
-
-        string_lines_list.append('')
-
-        string_lines_list.append(self._format_citations_string())
-
-        print()
-
-        return '\n'.join(string_lines_list)
+        return string
 
     def _format_authors_string(self):
         authors_string_list = []
@@ -108,31 +112,27 @@ class PublicationPrettyView(PublicationBriefView):
         return '\n'.join(authors_string_line_list)
 
     def _format_citations_string(self):
+
         citations_string_line_list = ['Cited by:']
 
         # Getting a list with Publication objects from the scopus id's given
         citation_publication_list = scopus_models.get_publication_list(self.publication.citations)
+        citation_string_list = []
 
         for citation_publication in citation_publication_list:
             # For every citation in the list of the papers, that cited the publication a bullet point is created so
             # that even when line wrapped, the strings have the same indent towards side
-            title = '{} ({})'.format(citation_publication.title, citation_publication.date)
-            lines = textwrap.wrap(title)
-            # Adding the bullet point to the start of the first line, if the lines were wrapped
-            bullet_point_string = self.bullet_point_style + ' '
-            lines[0] = bullet_point_string + lines[0]
-            # Adding the same indent
-            for index in range(1, len(lines)):
-                # Adding so many whitespaces to the additional lines, to accommodate them to the same level of indent
-                # as the line with the bullet point
-                whitespaces = ' ' * len(bullet_point_string)
-                lines[index] = whitespaces + lines[index]
-            lines.append('')
+            string = '{} ({})'.format(citation_publication.title, citation_publication.date)
+            citation_string_list.append(string)
 
-            # Adding the temp lines for a single citation bullet point to the lines list for the whole string
-            citations_string_line_list += lines
+        citation_bullet_point_string = format_bullet_point(citation_string_list,
+                                                           self.bullet_point_style,
+                                                           self.line_limit)
 
-        return '\n'.join(citations_string_line_list)
+        citations_string_line_list.append(citation_bullet_point_string)
+        citation_string = ''.join(citations_string_line_list)
+
+        return citation_string
 
 
 class AuthorBriefView:
