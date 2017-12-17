cc6915fe3cc78454775fef2ac77c43518a1b9ec8
the16thpythonist <jonseb1998@gmail.com>
2017-12-17 15:43:31 +0100

    Extended the documentation of the ScopusPublication Class

--- data.py
+++ data.py
@@ -1,6 +1,8 @@
 
 class ScopusPublication:
-
+    """
+    Object, that represents a publication, that was retrieved from the Scopus database.
+    """
     def __init__(self, scopus_id, eid, doi, title, description, date, creator, author_list, citation_list,
                  keyword_list, journal, volume):
 
@@ -19,6 +21,11 @@ class ScopusPublication:
 
     @property
     def affiliations(self):
+        """
+        The property, that will return the list of all affiliation ids, which the authors of the publication have.
+
+        :return: A list of int affiliation ids
+        """
         affiliation_list = []
         # Looping through the authors and adding all the affiliations, that have not already been added
         for author in self.authors:
@@ -36,6 +43,13 @@ class ScopusPublication:
         return int(self.id)
 
     def contains_author(self, scopus_author):
+        """
+        Whether or not the given author has participated in this publication.
+
+        :param scopus_author: The ScopusAuthor object representing the author, for which to check if he has worked on
+            the publication
+        :return: boolean
+        """
         for author in self.authors:
 
             # Comparing the id of the author object given with the author objects in the list of authors for the
@@ -47,7 +61,21 @@ class ScopusPublication:
         return False
 
     def contains_keyword(self, keyword):
+        """
+        Whether or not the publication is tagged with the given keyword.
+
+        :param keyword: The string keyword for which to check the publication
+        :return: boolean
+        """
         return keyword in self.keywords
 
     def contains_affiliation(self, affiliation_id):
+        """
+        Whether ot not the publication is affiliated with the given affiliation id.
+
+        Assembles a list of all different affiliation ids associated with all the authors of the publication and checks
+        if the given affiliation id is in this list.
+        :param affiliation_id: The int affiliation id to check for
+        :return: boolean
+        """
         return affiliation_id in self.affiliations
