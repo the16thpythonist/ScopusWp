2835744aa5a18299a6dab6497a73fa3f4b654b37
the16thpythonist <jonseb1998@gmail.com>
2017-12-18 04:53:01 +0100

    Renamed the former tests folder to 'prints' and created a new tests package and added a first test module for the scopus components

---
+++ tests/scopus_tests.py
@@ -0,0 +1,9 @@
+from ScopusWp.scopus.data import ScopusPublication, ScopusAuthor, ScopusAuthorProfile, ScopusAffiliation
+
+import pytest
+
+############################
+# THE DATA STRUCTURE TESTS #
+############################
+
+
