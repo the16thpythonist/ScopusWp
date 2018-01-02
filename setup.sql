69f10cae9eb69d6a187b6b9c679e39e59c2dd353
the16thpythonist <jonseb1998@gmail.com>
2018-01-02 21:51:08 +0100

    Added the creation of the unique indices to the sql setup code

--- setup.sql
+++ setup.sql
@@ -1,16 +1,14 @@
-# Creating the database for all the tables
-CREATE DATABASE scopus;
-USE scopus;
 
-CREATE TABLE reference(
-  id BIGINT,
-  wordpress_id BIGINT,
-  scopus_id BIGINT
+CREATE TABLE reference
+(
+    id BIGINT PRIMARY KEY,
+    wordpress_id BIGINT,
+    scopus_id BIGINT
 );
-
+CREATE UNIQUE INDEX reference_id_uindex ON reference (id);
 
 CREATE TABLE publications(
-  scopus_id BIGINT,
+  scopus_id BIGINT PRIMARY KEY,
   eid VARCHAR(32),
   doi VARCHAR(32),
   creator TEXT,
@@ -23,3 +21,6 @@ CREATE TABLE publications(
   keywords TEXT,
   citations TEXT
 );
+CREATE UNIQUE INDEX publications_scopus_id_uindex ON publications (scopus_id);
+CREATE UNIQUE INDEX publications_eid_uindex ON publications (eid);
+CREATE UNIQUE INDEX publications doi_unidex ON publications (doi);
