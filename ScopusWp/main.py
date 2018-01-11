845d87bf7e271b1793c1a79b4d05ec95c1cc6c8f
the16thpythonist <jonseb1998@gmail.com>
2018-01-11 08:56:00 +0100

    basic main impl. with important commands

--- ScopusWp/main.py
+++ ScopusWp/main.py
@@ -1,3 +1,5 @@
+from ScopusWp.controller import TopController
+
 import optparse
 
 
@@ -9,14 +11,24 @@ def main():
     if len(args) != 1:
         raise ValueError('Incorrect amount of arguments passed to ScopusWp')
 
-    if args[0] not in ['publications', 'citations', 'reload']:
+    if args[0] not in ['publications', 'citations', 'reload', 'wipe']:
         raise ValueError('Incorrect argument passed to Scopus Wp')
 
     if args[0] == 'publications':
-        pass
+        controller = TopController()
+        controller.update_publications_website()
 
     elif args[0] == 'citations':
-        pass
+        print('works')
 
     elif args[0] == 'reload':
-        pass
+        controller = TopController()
+        controller.reload_scopus_cache_observed()
+
+    elif args[0] == 'wipe':
+        controller = TopController()
+        controller.wipe_website()
+
+
+if __name__ == '__main__':
+    main()
