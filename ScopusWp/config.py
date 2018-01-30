d353d8fc83665c9f1b9605ce9f46533f53d19b5c
the16thpythonist <jonseb1998@gmail.com>
2018-01-30 09:21:52 +0100

    Made a logging controller, which makes the logging system extendable

--- ScopusWp/config.py
+++ ScopusWp/config.py
@@ -3,6 +3,7 @@ import logging
 import os
 
 import datetime
+import pathlib
 
 # Getting the absolute path to the folder of this very script file, so that can be used to open the config file, which
 # is also located in this folder
@@ -57,6 +58,88 @@ def init_logging():
                         filename=file_path,
                         filemode=file_mode,
                         format='%(asctime)s %(name)-40s %(levelname)-8s %(message)s')
+
+
+class LoggingController:
+
+    DATE_FORMAT = '%Y-%m-%d'
+    DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+
+    def __init__(self):
+        self.config = Config.get_instance()
+
+        self.log_file_path = pathlib.Path(LOGGING_PATH) / self.create_log_file_name()
+
+    def init(self):
+        self.prepare_log_file(self.log_file_path)
+
+    def close(self):
+        self.post_process_log_file(self.log_file_path)
+
+    def prepare_log_file(self, path):
+
+        # Checking if the path exists and if it does, that means there was a log written on that day already, then
+        # opening the file in append mode
+        # Getting the current datetime string
+        datetime_object = datetime.datetime.now()
+        datetime_string = datetime_object.strftime(self.DATETIME_FORMAT)
+
+        line_string = 'STARTING A NEW SESSION @ "{}"'.format(datetime_string)
+
+        if path.exists():
+            file_mode = 'a'
+        else:
+            file_mode = 'w'
+
+        with path.open(mode=file_mode) as file:
+            file.write(line_string)
+
+        file_path = str(path)
+        logging.basicConfig(
+            level=logging.INFO,
+            filename=file_path,
+            filemode=file_mode,
+            format='%(asctime)s %(name)-40s %(levelname)-8s %(message)s'
+        )
+
+    def post_process_log_file(self, path):
+        datetime_object = datetime.datetime.now()
+        datetime_string = datetime_object.strftime(self.DATETIME_FORMAT)
+
+        line_string = 'CLOSING SESSION @ "{}"'.format(datetime_string)
+
+        with path.open(mode='a') as file:
+            file.write(line_string)
+
+    def create_activity_file_name(self):
+        # Creating the date string from the current datetime object
+        datetime_object = datetime.datetime.now()
+        date_string = datetime_object.strftime(self.DATE_FORMAT)
+        # The base name extension for the file name is saved as a entry in the config file
+        base_name_string = self.config['LOGGING']['activity_log_name']
+
+        # Joining the date string and the base string into the log file name
+        name_string = '{}-{}.txt'.format(
+            date_string,
+            base_name_string
+        )
+
+        return name_string
+
+    def create_log_file_name(self):
+        # Creating the date string from the current datetime object
+        datetime_object = datetime.datetime.now()
+        date_string = datetime_object.strftime(self.DATE_FORMAT)
+        # The base name extension for the file name is saved as a entry in the config file
+        base_name_string = self.config['LOGGING']['debug_log_name']
+
+        # Joining the date string and the base string into the log file name
+        name_string = '{}-{}.txt'.format(
+            date_string,
+            base_name_string
+        )
+
+        return name_string
 
 
 class Config:
